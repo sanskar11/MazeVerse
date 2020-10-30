f28e10d6b17ef1aa64fca323a7ad923f88ffa961
sanskar11 <sanskartibrewal11@gmail.com>
2020-10-31 03:14:34 +0530

    holding c reduces health

--- Assets/camerachange.cs
+++ Assets/camerachange.cs
@@ -6,11 +6,13 @@ public class camerachange : MonoBehaviour
 {
  public Camera cam1;
  public Camera cam2;
-
+public Healthbar hb;
+private bool damage = false;
  
  void Start() {
      cam1.enabled = true;
      cam2.enabled = false;
+     hb = GameObject.Find("Healthbar").GetComponent<Healthbar>();
  }
  
  void Update() {
@@ -18,10 +20,16 @@ public class camerachange : MonoBehaviour
      if (Input.GetKeyDown(KeyCode.C)) {
          cam1.enabled = !cam1.enabled;
          cam2.enabled = !cam2.enabled;
+         damage=true;
      }
      if(Input.GetKeyUp(KeyCode.C)) {
          cam1.enabled = !cam1.enabled;
          cam2.enabled = !cam2.enabled;
+         damage=false;
+     }
+     if(damage==true)
+     {
+         hb.TakeDamage(2);
      }
  }
 }
