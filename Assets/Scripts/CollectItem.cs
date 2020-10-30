beb78743dfc338d416647e6db7b43f16d00acd14
sanskar11 <sanskartibrewal11@gmail.com>
2020-10-31 02:14:01 +0530

    Key taken UI added

--- Assets/Scripts/CollectItem.cs
+++ Assets/Scripts/CollectItem.cs
@@ -1,31 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectItem : MonoBehaviour
 {
     public bool keyCollected = false;
     List<GameObject> walls;
     public Healthbar hb;
-
+    public Image keyimg;
     private bool canReduceHealth = true;
     
     void Start(){
         walls = GameObject.Find("Main Camera").GetComponent<MazeLoader>().walls;
         hb = GameObject.Find("Healthbar").GetComponent<Healthbar>();
+        keyimg = GetComponent<Image>();
     }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         if(collisionInfo.collider.tag == "Key"){
-            Destroy(collisionInfo.gameObject);
             keyCollected = true;
+            Destroy(collisionInfo.gameObject);
+            Color temp = keyimg.color;
+            temp.a = 255;
+            keyimg.color=temp;
+
             hb.TakeDamage(50);
         }
         if(collisionInfo.collider.tag == "Door"){
+                Color tem = keyimg.color;
+                tem.a = 0.3f;
+                keyimg.color=tem;
             if(keyCollected)
             {
                 Destroy(collisionInfo.gameObject);
+                //Color temp = keyimg.color;
+                //temp.a = 50;
+                //keyimg.color=temp;
+                hb.TakeDamage(50);
             }
             else
             {
