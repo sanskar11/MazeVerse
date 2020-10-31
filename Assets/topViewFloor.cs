2f2e4fc70a03338e5510ca8a5ce654507385987c
sanskar11 <sanskartibrewal11@gmail.com>
2020-10-31 14:47:46 +0530

    Top View platform added

---
+++ Assets/topViewFloor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topViewFloor : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+    private bool canChange=true;
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+    void OnTriggerEnter()
+    {
+        cam2.enabled = true;
+    }
+
+    void OnTriggerExit()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+}
