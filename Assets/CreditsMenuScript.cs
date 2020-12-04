103171e0439184a2b9f2ad21a3b4b256bc7e8b24
trizonxjay <trizonxjay@gmail.com>
2020-12-04 17:49:23 +0530

    Added credits menu

---
+++ Assets/CreditsMenuScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenuScript : MonoBehaviour
+{
+    public void OpenLink(string link){
+        Application.OpenURL(link);
+    }
+}
